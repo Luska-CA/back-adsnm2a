aa5e793f78a03fc0cc426c2ce51ded26551148fd
Lucas Alves <lucas.calliari@iesb.edu.br>
2024-05-03 21:34:05 -0300

    aula09

--- aulas/aula09/index.js
+++ aulas/aula09/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const Produto = require('./modelo');
 
 const url = process.env.MONGODB_URL;
 
@@ -11,6 +12,64 @@ async function main(){
     }catch(err){
         console.log("Deu ruim", err.message);
     }
+
+    const produto = new Produto({
+        nome: 'banana',
+        preco: 12,
+        quantidade: 5
+    });
+    try{
+        await produto.save();
+        console.log(produto);
+    }catch (err){
+        for(let key in err.erros){
+            console.log(err.errors[key].message)
+        }
+    }
+
+ 
+
+/*    const produto = await Produto.create({
+        nome: 'uva',
+        preco: 32.5,
+        quantidade: 10
+    });
+
+    console.log(produto);
+*/
+
+/*
+    const produtos = await Produto.insertMany([
+        {nome: "maçã", preco: 20.7, quantidade: 8},
+        {nome: "pera", preco: 12.5, quantidade: 17},
+        {nome: "nabo", preco: 23.6, quantidade: 4}
+    ]);
+
+    console.log(produtos);
+*/
+/* 
+    const produto = await Produto.findOne({nome: "banana"});
+    console.log(produto);
+*/
+/*
+    const produtos = await Produto.find({nome:"banana"});
+    console.log(produtos);
+*/
+/*
+    const produto = await Produto.findOneAndUpdate(
+        {nome: "banana"},{nome: "banana nanica", preco: 15.0, quantidade: 20}
+    );
+        // const result = await Produto.uptadeOne({nome: "banana"}, {nome: "banana nanica", preco: 15, quantidade: 20});
+    console.log(produto);
+*/
+/*
+    const produto = await Produto.findOneAndDelete({nome: "uva"});
+    console.log(produto);
+*/
+
+//    const produto = await Produto.deleteMany({nome: "banana"})
+
+
 
     await mongoose.disconnect();
 }
--- aulas/aula09/modelo.js
+++ aulas/aula09/modelo.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const produtoSchema = new mongoose.Schema({
-    nome: String,
-    preco: Number,
-    quantidade: Number
+    nome: {
+        type: 
+        String,
+        trim: true,
+        uppercase: true,
+        required: [true, "Nome é obrigatório"],
+        minlenght: [3, "Nome deve conter pelo menos 3 caracteres"]
+    },
+
+    preco: {type: Number, min: [0, "Preço deve ser >= 0"]},
+    quantidade: {type: Number, default: 0}
 });
 
 module.exports = mongoose.model('Produto', produtoSchema);
