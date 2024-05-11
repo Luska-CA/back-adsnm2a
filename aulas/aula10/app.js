559631bddfb98b2247ff3f3f343c29e15265d4b4
Lucas Alves <lucas.calliari@iesb.edu.br>
2024-05-10 21:54:35 -0300

    aula10_continacao

--- aulas/aula10/app.js
+++ aulas/aula10/app.js
@@ -4,17 +4,19 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const routerApidocs = require('./routes/router_apidocs');
 const routerProdutos = require('./routes/router_produtos');
 
-var app = express();
-
 mongoose.connect(process.env.MONGODB_URL);
+
+var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use('/api-docs', routerApidocs);
 app.use('/produtos', routerProdutos);
 
 module.exports = app;
